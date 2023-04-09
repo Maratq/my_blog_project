47841052eacea5cb604ef9977743a4b530663a26
Marat <104265985+Maratq@users.noreply.github.com>
2023-04-09 12:14:44 +0300

    add project files

---
+++ my_blog/my_blog_app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+class Post(models.Model):
+    h1 = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    url = models.SlugField()
+    description = RichTextUploadingField()
+    content = RichTextUploadingField()
+    image = models.ImageField(default=None)
+    created_at = models.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Person(models.Model):
+    name = models.CharField(max_length=50,blank=True)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10,blank=True)
