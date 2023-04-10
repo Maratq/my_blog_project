43f391a04a1890216f7c2e6ec0b4adb0ceecc526
Marat <104265985+Maratq@users.noreply.github.com>
2023-04-10 18:15:26 +0300

    update urls registration

--- my_blog_files/my_blog_app/urls.py
+++ my_blog_files/my_blog_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MainView, PostDetailView
+from .views import MainView, PostDetailView, SignUpView
 
 urlpatterns = [
     path('', MainView.as_view(), name='home'),
     path('my_blog_app/<slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
