bbf536d4fd152c264d99f9365559d138a3b58492
Marat <104265985+Maratq@users.noreply.github.com>
2023-04-10 17:57:17 +0300

    fix

--- my_blog/my_blog/my_blog_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import MainView, PostDetailView
-
-urlpatterns = [
-    path('', MainView.as_view(), name='home'),
-    path('<slug>/', PostDetailView.as_view(), name='post_detail'),
-]
