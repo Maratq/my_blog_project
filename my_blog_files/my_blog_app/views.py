e34dd5602b8590f9fb0ef4eee070cea4a3aa6022
Marat <104265985+Maratq@users.noreply.github.com>
2023-04-10 18:10:55 +0300

    registration form processing

--- my_blog_files/my_blog_app/views.py
+++ my_blog_files/my_blog_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
 from .models import Post
+from .forms import SignUpForm
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
 
 
 class MainView(View):
@@ -23,3 +26,22 @@ class PostDetailView(View):
         return render(request, 'my_blog_app/post_detail.html', context={
             'post': post,
         })
+
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignUpForm()
+        return render(request, 'my_blog_app/signup.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+            return render(request, 'my_blog_app/signup.html', context={
+                'form': form,
+            })
