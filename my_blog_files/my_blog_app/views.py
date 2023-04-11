cc7ef4eef804f41ab1fc8cc9df3cad1ecead982e
Marat <104265985+Maratq@users.noreply.github.com>
2023-04-11 21:03:35 +0300

    add FeedbackForm

--- my_blog_files/my_blog_app/views.py
+++ my_blog_files/my_blog_app/views.py
@@ -2,9 +2,45 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
 from .models import Post
-from .forms import SignUpForm,SignInForm
+from .forms import SignUpForm, SignInForm, FeedbackForm
 from django.contrib.auth import login
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.mail import send_mail, BadHeaderError
+
+
+class FeedbackView(View):
+    def get(self, request, *args, **kwargs):
+        form = FeedbackForm()
+        return render(request, 'my_blog_app/contact.html', context={
+            'form': form,
+            'title': 'Написать мне',
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            from_email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+           #try:
+           #    send_mail(f'От {name} | {subject}', message, from_email, [''])
+           #except BadHeaderError:
+           #    return HttpResponse('Невалидный заголовок')
+            return HttpResponseRedirect('success')
+        return render(request, 'my_blog_app/contact.html', context={
+            'form': form,
+        })
+
+
+
+
+
+class SuccessView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'my_blog_app/success.html', context={
+            'title': 'Спасибо'
+        })
 
 
 class MainView(View):
@@ -45,6 +81,7 @@ class SignUpView(View):
             return render(request, 'my_blog_app/signup.html', context={
                 'form': form,
             })
+
 
 class SignInView(View):
     def get(self, request, *args, **kwargs):
