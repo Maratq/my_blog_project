4e306662ced7b0f69c7aabe8d0ddb1a67dbecab3
Marat <104265985+Maratq@users.noreply.github.com>
2023-04-18 17:52:58 +0300

    Update urls.py

--- my_blog_files/my_blog_app/urls.py
+++ my_blog_files/my_blog_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (
+from . views import (
     MainView, PostDetailView, SignUpView,SignInView,SuccessView,FeedbackView,SearchResultsView,TagView
 )
 from django.contrib.auth.views import LogoutView
